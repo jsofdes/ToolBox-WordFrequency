ca79bec59f0b7d87a5a71bafcef7d30d4887dcf9
jsofdes <Juanita.Desouza@students.olin.edu>
2017-03-17 01:37:22 -0400

    done see last iteration for prints/testing

--- frequency.py
+++ frequency.py
@@ -2,7 +2,7 @@
 Project Gutenberg """
 
 import string
-
+#Scarlet Letter
 
 def get_word_list(file_name):
     """ Reads the specified project Gutenberg book.  Header comments,
@@ -17,39 +17,26 @@ def get_word_list(file_name):
     while lines[curr_line].find('START OF THIS PROJECT GUTENBERG EBOOK') == -1:
             curr_line += 1
     lines = lines[curr_line+1:]
-            #print(lines)
-    lines_total=''
-    print('bb')
-    #list1 = filter(lambda x: x != '\n', lines)
-    print('so')
-    #print(list1)
+    lines_total = ''
     for line in lines:
-            #while line != str("End of Project Gutenberg's The Scarlet Letter, by Nathaniel Hawthorne\n"):
                 if line != '\n':
                     lines_total = lines_total + line
-    #print(lines_total)
     lines_total_split = lines_total.split()
-    print(lines_total_split)
-
+    # remove ending
     for i in range(0, len(lines_total_split)):
         if (lines_total_split[i] == 'End') and (lines_total_split[i+1] == 'of') and (lines_total_split[i+2] == 'Project'):
                 break
-            #print(i)
-    lines_total_s=lines_total_split[0:i]
-    print(lines_total_s)
-    final_set=[]
-    for x in range (0,i):
+    # create array
+    lines_total_s = lines_total_split[0:i]
+    final_set = []
+    # change array so doesn't include end
+    for x in range(0, i):
         a = lines_total_s[x].strip(string.punctuation)
-        b=a.lower()
+        b = a.lower()
         final_set.append(b)
-    print (final_set)
+    # print (final_set)
     word_list = final_set
     return word_list
-
-
-            # "End of Project Gutenberg's The Scarlet Letter, by Nathaniel Hawthorne\n"
-
-
 
 
 def get_top_n_words(word_list, n):
@@ -62,10 +49,9 @@ def get_top_n_words(word_list, n):
     returns: a list of n most frequently occurring words ordered from most
     frequently to least frequentlyoccurring
     """
-    word_dict=dict()
+    word_dict = dict()
     for word in word_list:
-       word_dict[word] = word_dict.get(word, 0) + 1
-
+        word_dict[word] = word_dict.get(word, 0) + 1
     ordered_by_frequency = sorted(word_dict, key=word_dict.get, reverse=True)
     print(ordered_by_frequency[0: n])
 
@@ -74,5 +60,5 @@ if __name__ == "__main__":
     print("Running WordFrequency Toolbox")
     print(string.punctuation)
 
-    b= get_word_list('h_full.txt')
+    b = get_word_list('h_full.txt')
     get_top_n_words(b, 10)
